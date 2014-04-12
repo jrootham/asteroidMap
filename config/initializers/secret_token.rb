567c01c7c4761248539c2d6e0500a45f513e36b1
ojordan <contact@ojordan.tk>
2014-04-12 15:26:53 -0400

    initial commit with ruby code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsteroidMaps::Application.config.secret_key_base = '6a0ccac7ed962cc6dde5c711c5ebe69bc484d799da3ed1e472cb23d0fc2e9b2b4d03d45c41060b73669d3b5e15e2cc89cd8966d72c5d7e76db05638ae2f8a304'
